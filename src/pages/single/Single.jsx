4af8eab3b8e059ca6a8bb97f0d65042cb21302da
Leandro Freire <fotografoleandro81@gmail.com>
2023-01-28 13:30:01 -0300

    single

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,8 +3,14 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const Single = () => {
+  const params =useParams()
+  const { data } = useSelector(state => state)
+  let user = data.find(r => r.id===params.userId)
+  console.log('single: ', user)
   return (
     <div className="single">
       <Sidebar />
@@ -13,39 +19,35 @@ const Single = () => {
         <div className="top">
           <div className="left">
             <div className="editButton">Edit</div>
-            <h1 className="title">Information</h1>
+            <h1 className="title">Información individual</h1>
             <div className="item">
-              <img
+{/*               <img
                 src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
                 alt=""
                 className="itemImg"
-              />
+              /> */}
               <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
+                <h1 className="itemTitle">{user?.full_name}</h1>
                 <div className="detailItem">
-                  <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemKey">E-mail:</span>
+                  <span className="itemValue">{user.email}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Phone:</span>
-                  <span className="itemValue">+1 2345 67 89</span>
+                  <span className="itemKey">Teléfono:</span>
+                  <span className="itemValue">{user.celular}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Address:</span>
+                  <span className="itemKey">Red social:</span>
                   <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
+                    <a href={user.user}>{user.user}</a>
                   </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Country:</span>
-                  <span className="itemValue">USA</span>
                 </div>
               </div>
             </div>
           </div>
-          <div className="right">
+{/*           <div className="right">
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
-          </div>
+          </div> */}
         </div>
         <div className="bottom">
         <h1 className="title">Last Transactions</h1>
